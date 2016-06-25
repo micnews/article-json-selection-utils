a89f16bdfd6e2b7eab42bb052b2481ff37b62772
David Björklund <david.bjorklund@gmail.com>
2016-06-25 14:28:46 +0200

    return all items in getSelectionItems

--- test/get-selection-items-test.js
+++ test/get-selection-items-test.js
@@ -20,15 +20,22 @@ test('getSelectionItems() no selection', t => {
 test('getSelectionItems() simple selection', t => {
   const articleJson = [{
     type: 'paragraph',
+    children: [{content: 'beep boop'}]
+  }, {
+    type: 'paragraph',
     children: [
       {mark: true, markClass: 'selection-start'},
       {content: 'hello, world!'},
       {mark: true, markClass: 'selection-end'}
     ]
+  }, {
+    type: 'paragraph',
+    children: [{content: 'foo bar'}]
   }];
-  const actual = getSelectionItems(articleJson)[0];
-  const expected = articleJson[0];
-  t.is(actual, expected);
+  const actual = getSelectionItems(articleJson);
+  const expectedItem = articleJson[1];
+  t.is(actual.length, 1, 'should return 1 element');
+  t.is(actual[0], expectedItem);
 });
 
 test('getSelectionItems() embed', t => {
@@ -37,5 +44,29 @@ test('getSelectionItems() embed', t => {
     embedType: 'image'
   }]);
   const expected = [];
+  t.deepEqual(actual, expected);
+});
+
+test('getSelectionItems() before & after embed', t => {
+  const articleJson = [
+    {
+      type: 'paragraph',
+      children: [
+        {mark: true, markClass: 'selection-start'},
+        {content: 'hello,'}
+      ]
+    }, {
+      type: 'embed',
+      embedType: 'image'
+    }, {
+      type: 'paragraph',
+      children: [
+        {content: 'world!'},
+        {mark: true, markClass: 'selection-end'}
+      ]
+    }
+  ];
+  const actual = getSelectionItems(articleJson);
+  const expected = articleJson;
   t.deepEqual(actual, expected);
 });
